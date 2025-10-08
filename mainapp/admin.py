0d1058bf91b8e2b13dfab72e8297b59f88613641
lopezjua <juancarlos.lopez@gi-de.com>
2025-10-07 19:26:03 -0600

    Defining the Entry Model

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from mainapp.models import Topic
+from mainapp.models import Topic, Entry
 
 # Register your models here.
 admin.site.register(Topic)
+admin.site.register(Entry)
