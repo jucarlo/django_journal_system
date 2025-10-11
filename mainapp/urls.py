30e9d9c03c1bcfa15d1db2c3eca1e83deb8bd2de
lopezjua <juancarlos.lopez@gi-de.com>
2025-10-10 22:17:08 -0600

    Add individual Topic page

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -10,5 +10,8 @@ urlpatterns = [
     path("", views.index, name="index"),
 
     #Show all topics
-    path('topics', views.topics, name='topics')
+    path('topics', views.topics, name='topics'),
+
+    #Detail page for a single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
